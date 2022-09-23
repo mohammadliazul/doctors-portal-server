84dbc1c80f2660eae5b11683cbe7e5ec8264f79c
mohammadliazul <liazulislam0@gmail.com>
2022-09-23 11:12:23 +0600

    check user admin or not (get api)

--- index.js
+++ index.js
@@ -61,6 +61,17 @@ async function run() {
           res.json(result);
       });
 
+      app.get('/users/:email', async(req, res) => {
+        const email = req.params.email;
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        let isAdmin = false;
+        if(user?.role === 'admin'){
+          isAdmin = true;
+        }
+        res.json({admin: isAdmin});
+      });
+
     } finally {
     //   await client.close();
     }
